4497c74d9d639a04205bbfb6d4ab94be727c73f5
yurasheremeta <37277271+yurasheremeta@users.noreply.github.com>
2018-03-12 00:47:46 +0200

    Add files via upload

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+
+            //ViewBag.Greeting = hour > 00 
+            return View("MyView");
+           
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
---
+++ WebApplication2/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Pleace enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter uour email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage ="Please enter your mobile phone")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage ="Pleace specify wheather you will attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
